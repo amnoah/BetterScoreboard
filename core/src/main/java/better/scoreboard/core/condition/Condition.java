b60eb1fad1c627d6023aa46bf5e17f5e09b94ee9
Noah <spiritand564@gmail.com>
2025-01-06 02:12:11 -0500

    Fix: Update Condition Syntax

--- core/src/main/java/better/scoreboard/core/condition/Condition.java
+++ core/src/main/java/better/scoreboard/core/condition/Condition.java
@@ -28,8 +28,8 @@ public class Condition {
         if (config.getObject(String.class, "mode", "and").equalsIgnoreCase("or")) this.mode = Mode.OR;
         else this.mode = Mode.AND;
 
-        falseLine = new Line(plugin, config.getObject(String.class, "false-result", null));
-        trueLine = new Line(plugin, config.getObject(String.class, "true-result", null));
+        falseLine = new Line(plugin, config.getObject(String.class, "no", null));
+        trueLine = new Line(plugin, config.getObject(String.class, "yes", null));
     }
 
     public boolean isTrue(User user) {
